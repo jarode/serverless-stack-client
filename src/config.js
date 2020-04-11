c95fef46782bfc551ad2e222d55ac1b20c9c8c61
unknown <Jerry@HOISTPL-376.hoist.de>
2020-04-11 13:29:16 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "jone-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://veewguc883.execute-api.eu-central-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_RdO4CX9vH",
+    APP_CLIENT_ID: "173q0oruh2kf0n5fglo6i6a8ib",
+    IDENTITY_POOL_ID: "eu-central-1:0b59ff9d-225b-4649-bade-1a7378179e43"
+  }
+};
